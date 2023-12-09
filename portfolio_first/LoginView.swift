7393e1453e82de3af0549d0b14eb4f8521dc161b
daisuke.abe <commityneo@gmail.com>
2023-12-09 18:53:19 +0900

    ログイン画面を追加

---
+++ portfolio_first/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  portfolio_first
+//
+//  Created by 阿部大輔 on 2023/12/09.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
